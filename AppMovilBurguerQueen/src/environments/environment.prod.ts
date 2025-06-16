5e410a93717e479c2575bae6ac1c745dc0a00ccf
NeoFG <fernando.garza1@alumno.buap.mx>
2025-06-16 17:26:37 -0600

    Cargando proyecto burger

---
+++ AppMovilBurguerQueen/src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  urlApi: 'http://172.25.224.1:3000/api/v1/', //Pegamos la ip de nuestra pc
+  publishKey: '', //Cambiar llaves
+  secretKey: ''
+};
